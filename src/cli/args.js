6ebfd6528c96ab217b603f8530ebc9ec2bb2f2d0
anny_more <anxxxen@ya.ru>
2022-12-06 00:05:11 +0300

    feat: implement functions on 'cli' folder

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,17 @@
 const parseArgs = () => {
-    // Write your code here 
+
+    try {
+        const argParsed = process.argv.slice(2).join(' ').split('--').slice(1).map(arg => {
+            const [name, value] = arg.split(' ');
+            return `${name} is ${value}`;
+        })
+     
+        console.log(argParsed.join(', '));
+
+    } catch(err) {
+        throw err
+    }
+    
 };
 
 parseArgs();
